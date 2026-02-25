27aa3a0bbff8d653bbcfeb2d5450107a50bab8a1
milhosin <milho.of@gmail.com>
2026-02-24 21:23:17 -0300

    Adição de efeito de dano - Inimigo com animação

--- Assets/Script/Enemy/EnemyBase.cs
+++ Assets/Script/Enemy/EnemyBase.cs
@@ -6,6 +6,9 @@ public class EnemyBase : MonoBehaviour
 {
     public int damage = 10;
 
+    public Animator animator;
+    public string triggerAttack = "Attack";
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.transform.name);
@@ -15,6 +18,12 @@ public class EnemyBase : MonoBehaviour
         if(health != null)
         {
             health.Damage(damage);
+            PlayAttackAimation();
         }
+    }
+
+    private void PlayAttackAimation()
+    {
+        animator.SetTrigger(triggerAttack);
     }
 }
