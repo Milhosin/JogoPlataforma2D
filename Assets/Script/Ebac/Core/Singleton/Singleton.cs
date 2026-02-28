f9508db738599a3e5dd7ba8dc883d951ae432d56
milhosin <milho.of@gmail.com>
2026-02-28 00:27:19 -0300

    Adição Sistema de UI para Coins

--- Assets/Script/Ebac/Core/Singleton/Singleton.cs
+++ Assets/Script/Ebac/Core/Singleton/Singleton.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace Ebac.Core.Singleton
 {
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
