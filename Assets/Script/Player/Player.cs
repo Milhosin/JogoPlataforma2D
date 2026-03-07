3f1a96385465f86c1cc1c8dd067ab67310105aad
milhosin <milho.of@gmail.com>
2026-03-07 12:21:54 -0300

    Adição de VFX para o Jump

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -17,6 +17,12 @@ public class Player : MonoBehaviour
 
     private Animator _currentPlayer;
 
+    [Header("Jump Collision Check")]
+    public Collider2D collider2D;
+    public float distToGround;
+    public float spaceToGround = .1f;
+    public ParticleSystem jumpVFX;
+
     private void Awake()
     {
         if(healthBase != null)
@@ -25,6 +31,17 @@ public class Player : MonoBehaviour
         }
 
         _currentPlayer = Instantiate(soPlayerSetup.player, transform);
+
+        if(collider2D != null)
+        {
+            distToGround = collider2D.bounds.extents.y;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
     }
     
     private void OnPlayerKill()
@@ -37,6 +54,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        IsGrounded();
         HandleJump();
         HandleMoviment();
     }
@@ -84,7 +102,7 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
         {
             myRigidbody.velocity = Vector2.up * soPlayerSetup.forceJump;
             myRigidbody.transform.localScale = Vector2.one;
@@ -92,7 +110,13 @@ public class Player : MonoBehaviour
             DOTween.Kill(myRigidbody.transform);
 
             HandlescaleJump();
+            PlayerJumpVFX();
         }
+    }
+
+    private void PlayerJumpVFX()
+    {
+        if (jumpVFX != null) jumpVFX.Play();
     }
 
     private void HandlescaleJump()
