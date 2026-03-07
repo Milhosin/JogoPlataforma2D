73bd476923b519d0ffa2ef4cc6c5ea24526d8b61
milhosin <milho.of@gmail.com>
2026-03-07 11:29:20 -0300

    Adição de Particulas de poeira para o andar do player

--- Assets/Script/Coin/ItemCollactableBase.cs
+++ Assets/Script/Coin/ItemCollactableBase.cs
@@ -11,7 +11,7 @@ public class ItemCollactableBase : MonoBehaviour
 
     private void Awake()
     {
-        if (particleSystem != null) particleSystem.transform.SetParent(null);
+        //if (particleSystem != null) particleSystem.transform.SetParent(null);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
